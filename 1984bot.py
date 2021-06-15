70c8f78bb0497fbdd6753ed0b10a85a688f9beaa
dinolord919 <85969095+dinolord919@users.noreply.github.com>
2021-06-15 19:58:35 -0400

    Add files via upload
    Initial outline of features and planned logic

---
+++ 1984bot.py
@@ -0,0 +1,75 @@
+import discord
+import numpy as np
+
+
+
+'''
+DISCORD BOT
+- Word Highlight (Maybe find some way to fit into other blacklist too)
+- Cone/Kick/Ban message
+- Reaction Roles / Role Commands
+- Add/Remove blacklist
+- Join/Leave message
+'''
+
+'''
+Blacklist structure
+    Message containing blacklist
+    File containing blacklist and keywords [class, topic, 'clarification', [keywords]]
+        on initialization, all keywords added to a list
+    Add command: classification, topic, clarification, keywords
+    remove command: class and index/topic
+    Edit command: class, topic, revision field 1, etc
+        Revision field can be skipped with ^ character
+    View command: class, topic; returns keywords
+'''
+
+'''
+Rules structure
+    Message containing rules
+    Add command: 'rule'(, reindex number)
+    Remove command: index
+    Edit command: index, 'rule'
+'''
+
+'''
+Word Highlight
+    for keyword in blacklist:
+        if msgcontent.contains(keyword):
+            violationList.append(keyword)
+    send message in mod channel "message (copy) violates these keywords: [violationList]"
+'''
+
+'''
+Cone/Ice
+    log cone/ice update
+    if being added, send message in mod channel "hey! user just got coned/iced! length and reason?"
+    upon reply, post reason and punishment in #rulebreaker-central
+    set timer for length, automatically remove cone/ice at end
+        length can be omitted with ^ character
+'''
+
+'''
+Kick/ban
+    log kick/ban
+    send mod channel msg "hey! user got kicked/banned! reason?"
+    dm user with reason
+'''
+
+'''
+Join/Leave
+    on join, dm members a copy of the rules
+        add string "Randomly placed string to thwart bots! paste this into #shoelace to join" in certain spots randomly
+    append user and random string added to list
+    a msg from user containing string gives member role and removes them from list
+    purge list every 24 hours
+    on leave, immortalize their shame.
+'''
+
+'''
+Reaction Roles
+lol no idea how this works
+'''
+
+
+    
