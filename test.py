fcade02b4b68ad23874df5690fc27653804e1d9a
ArcWandx86 <57409597+ArcWandx86@users.noreply.github.com>
2021-07-21 13:54:16 -0400

    more testing options

--- test.py
+++ test.py
@@ -60,8 +60,9 @@ def results():
     
     offset = 0
     for key in iHighlight:
-        telemetry(' ', string, (key, iHighlight[key]), offset)
-        toAdd = f'[{string[key + offset:iHighlight[key] + offset]}](https://www.youtube.com/watch?v=dQw4w9WgXcQ)'
+        # telemetry(' ', string, (key, iHighlight[key]), offset)
+        # toAdd = "_"*(iHighlight[key] - key)
+        toAdd = f'[{string[key + offset:iHighlight[key] + offset]}]' # (https://www.youtube.com/watch?v=dQw4w9WgXcQ)'
         string = string[:key + offset] + toAdd + string[iHighlight[key] + offset:]
         offset += len(toAdd) + key - iHighlight[key]
     
